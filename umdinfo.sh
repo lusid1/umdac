b50906f74fcd66eb4b6e5584c707cc7e94ebc58b
lusid1 <sean_hatfield@hotmail.com>
2026-08-29 22:05:07 -0700

    small fix to suggested filename

--- umdinfo.sh
+++ umdinfo.sh
@@ -8,18 +8,18 @@ UMD_AUDIO=$(isoinfo -i "$iso" -x /UMD_AUDIO/PARAM.SFO 2>/dev/null | strings | ta
 filename=$(basename "$iso")
 
 # The longest title is probably the right one
-TITLE="$UMD_DATA"
-if [ ${#UMD_VIDEO} -gt ${#TITLE} ]; then TITLE="$UMD_VIDEO"; fi
+TITLE="$UMD_VIDEO"
+if [ ${#UMD_DATA} -gt ${#TITLE} ]; then TITLE="$UMD_DATA"; fi
 if [ ${#UMD_AUDIO} -gt ${#TITLE} ]; then TITLE="$UMD_AUDIO"; fi
 
-AUDIO_TRACKS=$(./umd2mkv -iso "$iso" -inspect | grep Audio | cut -d':' -f2 )
+AUDIO_TRACKS=$(umd2mkv -iso "$iso" -inspect | grep Audio | cut -d':' -f2 )
 Langs=$(echo $AUDIO_TRACKS | tr '=' ' ' | cut -d' ' -f2,5 | tr ' ' ',' | sed 's/.*/(&)/') 
 
 CRC32=$(crc32 "$iso") 
 
 echo ""
 echo "Filename.......: $filename"
-echo "Suggested......: $(echo "$TITLE" | tr ' ' '_' | tr -d '[:punct:]') $Langs[$CRC32].iso"
+echo "Suggested......: $(echo "$TITLE" | sed 's/[^A-Za-z0-9 _-]/_/g') $Langs[$CRC32].iso"
 echo "UMD_DATA Title.: $(isoinfo -i "$iso" -x /UMD_DATA.BIN | strings | cut -d'|' -f1)"
 if [ -n "$UMD_VIDEO" ]; then
     echo "UMD_VIDEO Title: $(isoinfo -i "$iso" -x /UMD_VIDEO/PARAM.SFO | strings | tail -n 1)"
@@ -29,7 +29,7 @@ if [ -n "$UMD_AUDIO" ]; then
 fi
 
 echo "Audio Tracks...:" $AUDIO_TRACKS
-echo "Subtitles......:" $(./umd2mkv -iso "$iso" -inspect | grep Subtitle | cut -d':' -f2 | paste -sd, -) 
+echo "Subtitles......:" $(umd2mkv -iso "$iso" -inspect | grep Subtitle | cut -d':' -f2 | paste -sd, -) 
 echo "CRC32..........:" $CRC32 
 echo "MD5............:" $(md5sum "$iso" | awk '{ print $1 }') 
 echo "SHA1...........:" $(sha1sum "$iso" | awk '{ print $1 }') 
