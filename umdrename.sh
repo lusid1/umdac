7a60b4e31d8d015bcff07ccebc07e77b9056632b
lusid1 <sean_hatfield@hotmail.com>
2026-08-30 15:12:31 -0700

    fix short title names

--- umdrename.sh
+++ umdrename.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-
 iso=$1
+
 
 UMD_DATA=$(isoinfo -i "$iso" -x /UMD_DATA.BIN 2>/dev/null | strings -n 1| cut -d'|' -f1 | sed 's/[[:space:]]*$//')
 UMD_VIDEO=$(isoinfo -i "$iso" -x /UMD_VIDEO/PARAM.SFO 2>/dev/null | strings -n 1| tail -n 1 | sed 's/[[:space:]]*$//' )
@@ -16,6 +16,7 @@ if [ ${#UMD_DATA} -gt ${#TITLE} ]; then TITLE="$UMD_DATA"; fi
 if [ ${#UMD_AUDIO} -gt ${#TITLE} ]; then TITLE="$UMD_AUDIO"; fi
 
 AUDIO_TRACKS=$(umd2mkv -iso "$iso" -inspect | grep Audio | cut -d':' -f2 )
+#Langs=$(echo $AUDIO_TRACKS | tr '=' ' ' | cut -d' ' -f2,5 | tr ' ' ',' | sed 's/.*/(&)/') 
 Langs=$(
     echo "$AUDIO_TRACKS" \
     | sed 's/, /\n/g' \
@@ -25,7 +26,7 @@ Langs=$(
     | sed 's/,$//' \
     | sed 's/.*/(&)/'
 )
-
+echo "Processing $iso"
 CRC32=$(crc32 "$iso" |cut -f1) 
 #suggested_name="$(echo "$TITLE" | sed 's/[^A-Za-z0-9 _-()]//g' | sed 's/[[:space:]]*$//') $Langs[$CRC32].iso"
 suggested_name="$(
@@ -34,16 +35,9 @@ suggested_name="$(
     | sed 's/[[:space:]]*$//'
 ) $Langs[$CRC32].iso"
 
-echo ""
-echo "File Name......: $filename"
-echo "Suggested Name.: $suggested_name"
-echo "UMD_DATA Title.: $UMD_DATA"
-echo "UMD_VIDEO Title: $UMD_VIDEO"
-echo "UMD_AUDIO Title: $UMD_AUDIO"
-echo "Audio Tracks...:" $AUDIO_TRACKS
-echo "Subtitles......:" $(umd2mkv -iso "$iso" -inspect | grep Subtitle | cut -d':' -f2 | paste -sd, -) 
-echo "CRC32..........:" $CRC32 
-echo "MD5............:" $(md5sum "$iso" | awk '{ print $1 }') 
-echo "SHA1...........:" $(sha1sum "$iso" | awk '{ print $1 }') 
-echo "Size in bytes..:" $(wc -c < "$iso") 
-echo ""
+if [ -f "$filepath/$suggested_name" ]; then
+    echo "File $suggested_name already exists, skipping rename"
+    exit 1
+fi
+echo "renaming $filename to $suggested_name"
+mv "$iso" "$filepath/$suggested_name"
